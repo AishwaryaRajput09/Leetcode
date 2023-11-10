dc284c523e101ca6647bc7cd185177dba5aa5537
AishwaryaRajput09 <71898668+AishwaryaRajput09@users.noreply.github.com>
2023-11-10 07:32:14 +0530

    Time: 15 ms (20.64%) | Memory: 6.4 MB (20.32%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+       long long int sum = 0;
+        int original = x;
+        while(x > 0){
+            int rem = x % 10;
+            sum = sum * 10 + rem;
+            x = x / 10;
+        }
+        if(sum == original && original >= 0){
+           return true;
+        }else{
+            return false;
+        }
+        
+    }
+};
