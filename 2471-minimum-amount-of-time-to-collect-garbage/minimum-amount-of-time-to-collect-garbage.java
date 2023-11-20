a6c3c6f37a7bd011eb425683b25f212e9fa367d6
AishwaryaRajput09 <71898668+AishwaryaRajput09@users.noreply.github.com>
2023-11-20 14:56:45 +0530

    Time: 3 ms (96.97%) | Memory: 62.3 MB (36.74%) - LeetSync

--- 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.java
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.java
@@ -1,44 +1,21 @@
 class Solution {
     public int garbageCollection(String[] garbage, int[] travel) {
-        int time = 0;
-        int n = garbage.length;
-        int G_indx = 0;
-        int P_indx = 0;
-        int M_indx = 0;
+        int res = 0;
 
-        int[] prefixSum=new int[travel.length];
-        prefixSum[0] = travel[0];
-        for(int i=1; i<travel.length; i++) {
-            prefixSum[i] = travel[i] + prefixSum[i-1];
+        for (String g : garbage) {
+            res += g.length();
         }
 
-         
-        for(int i=0; i<n; i++) {
-            String temp = garbage[i];
-            time += temp.length();
-            for(int j=0; j<temp.length(); j++) {
-                if(temp.charAt(j) == 'M') {
-                    M_indx = i;
-                }
-                else if(temp.charAt(j) == 'P') {
-                    P_indx = i;
-                }
-                else if(temp.charAt(j)== 'G'){
-                    G_indx = i;
-                }
-            }
+        boolean m = false, p = false, g = false;
+
+        for (int i = travel.length; i > 0; i--) {
+            m = m || garbage[i].contains("M");
+            p = p || garbage[i].contains("P");
+            g = g || garbage[i].contains("G");
+
+            res += travel[i-1] * ((m ? 1 : 0) + (p ? 1 : 0) + (g ? 1 : 0));
         }
 
-         if (M_indx > 0) {
-        time += prefixSum[M_indx-1];
-        }
-        if (P_indx > 0) {
-            time += prefixSum[P_indx-1];
-        }
-        if (G_indx > 0) {
-            time += prefixSum[G_indx-1];
-        }
-        return time;
-    
+        return res;      
     }
 }
