bfa9d50aa3f5584d3c8c2673825d1937ce0493b9
AishwaryaRajput09 <71898668+AishwaryaRajput09@users.noreply.github.com>
2023-11-25 21:43:49 +0530

    Time: 1 ms (99.92%) | Memory: 43.7 MB (43.41%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.java
@@ -0,0 +1,34 @@
+
+
+public class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> currentSubset = new ArrayList<>();
+        Arrays.sort(nums); // Sort the array to handle duplicates
+        subsetsHelper(nums, n, ans, currentSubset, 0);
+        return ans;
+    }
+
+    static List<List<Integer>> subsetsHelper(int[] nums, int n, List<List<Integer>> ans, List<Integer> currentSubset, int ind) {
+        if (ind == n) {
+            ans.add(new ArrayList<>(currentSubset));
+            return ans;
+        }
+
+        // Include the current element in the subset
+        currentSubset.add(nums[ind]);
+        subsetsHelper(nums, n, ans, currentSubset, ind + 1);
+
+        // Skip duplicates
+        while (ind < n - 1 && nums[ind] == nums[ind + 1]) {
+            ind++;
+        }
+
+        // Exclude the current element from the subset
+        currentSubset.remove(currentSubset.size() - 1);
+        subsetsHelper(nums, n, ans, currentSubset, ind + 1);
+
+        return ans;
+    }
+}
