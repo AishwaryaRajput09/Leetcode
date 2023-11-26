beb64e974860f4757cb1b6bae1b2b6b76d7e2619
AishwaryaRajput09 <71898668+AishwaryaRajput09@users.noreply.github.com>
2023-11-26 13:20:36 +0530

    Time: 6 ms (96.75%) | Memory: 55.5 MB (36.42%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        List<String> ans = new ArrayList<>();
+        palinS(res, ans, s, 0);
+        return res;
+    }
+
+    void palinS(List<List<String>> res, List<String> ans, String s, int start) {
+        if (start == s.length()) {
+            res.add(new ArrayList<>(ans));
+            return;
+        }
+
+        for (int end = start + 1; end <= s.length(); end++) {
+            String substring = s.substring(start, end);
+            if (isPalindrome(substring)) {
+                ans.add(substring);
+                palinS(res, ans, s, end);
+                ans.remove(ans.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
