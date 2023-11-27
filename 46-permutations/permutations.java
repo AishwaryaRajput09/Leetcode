83c85c3020c5018dc792ec0f965057d83b9c0212
AishwaryaRajput09 <71898668+AishwaryaRajput09@users.noreply.github.com>
2023-11-27 15:36:41 +0530

    Time: 1 ms (94.40%) | Memory: 43.8 MB (57.27%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,33 +1,31 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> ans = new ArrayList<>();
-        int n = nums.length-1;
-        permutations(nums,res,ans,0,n);
-        return res;
+    List<List<Integer>> res = new ArrayList<>();
+    perm(nums,res,0);
+    return res;
     }
-    void permutations(int[] nums,List<List<Integer>> res ,List<Integer> ans,int i, int n){
-            if(i==nums.length){
-                for(int k = 0; k < nums.length;k++){
-                    ans.add(nums[k]);
-                }
-                res.add(new ArrayList<>(ans));
-                ans.clear();
-                return;
+    private void perm(int[] nums,List<List<Integer>> res,int ind){
+        if(ind == nums.length){
+            List<Integer> ans = new ArrayList<>();
+            for(int e : nums){
+                ans.add(e);
             }
-            // int k = ans.get(i);
-            for(int j = i ; j < nums.length;j++){
-                swap(j,i,nums);
-                permutations(nums,res,ans,i+1,n);
-                swap(j,i,nums);
-
-            }
-        
-         
+            res.add(new ArrayList<>(ans));
+            return;
+        }
+        for(int i = ind; i<nums.length;i++){
+            int temp = nums[i];
+            nums[i] = nums[ind];
+            nums[ind]=temp;
+            perm(nums,res,ind+1);
+            temp = nums[i];
+            nums[i] = nums[ind];
+            nums[ind]=temp;
+        }
     }
-     void swap(int i, int j , int [] nums){
-             int temp = nums[i];
-             nums[i] = nums[j];
-             nums[j] = temp;
-         }
+    private void swap(int i, int j, int[]  nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
