a5b491465a9328d7e11c737f82f41ffc1251925d
AishwaryaRajput09 <71898668+AishwaryaRajput09@users.noreply.github.com>
2023-12-02 09:32:32 +0530

    Time: 2 ms (100.00%) | Memory: 44.5 MB (15.19%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.java
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.java
@@ -1,38 +1,29 @@
 class Solution {
     public int countCharacters(String[] words, String chars) {
-        int count = 0;
-        
-        int[] charCount = new int[26]; // Assuming lowercase English letters
-        
-        // Count the frequency of each character in chars
-        for (char c : chars.toCharArray()) {
-            charCount[c - 'a']++;
+        int[] counts = new int[26];
+        for(int i=0;i<chars.length();i++)
+        {
+            counts[chars.charAt(i)-'a']++;
         }
-
-        // Check if each word in words can be formed using chars
-        for (String word : words) {
-            int[] wordCount = new int[26];
-
-            // Count the frequency of each character in the current word
-            for (char c : word.toCharArray()) {
-                wordCount[c - 'a']++;
-            }
-
-            // Check if the current word can be formed using chars
-            boolean canFormWord = true;
-            for (int i = 0; i < 26; i++) {
-                if (wordCount[i] > charCount[i]) {
-                    canFormWord = false;
-                    break;
-                }
-            }
-
-            // If the word can be formed, add its length to the count
-            if (canFormWord) {
-                count += word.length();
-            }
+        int res =0 ;
+        for(String s : words)
+        {
+            if(canForm(s,counts))
+                res+=s.length();
         }
+        return res;
+    }
 
-        return count;
+    boolean canForm(String word, int[] counts)
+    {
+        int[] c = new int[26];
+        for(int i =0;i<word.length();i++)
+        {
+            int x = word.charAt(i)-'a';
+            c[x]++;
+            if(c[x] > counts[x])
+                return false;
+        }
+        return true;
     }
 }
