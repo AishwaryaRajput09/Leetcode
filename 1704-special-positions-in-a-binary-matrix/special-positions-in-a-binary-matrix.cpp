0c9b003868ddd091c775c01ced9bc41cb9c36b82
AishwaryaRajput09 <71898668+AishwaryaRajput09@users.noreply.github.com>
2023-12-13 07:50:51 +0530

    Time: 17 ms (54.47%) | Memory: 13.2 MB (70.53%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -2,27 +2,28 @@ class Solution {
 public:
     int numSpecial(vector<vector<int>>& mat) {
         int count = 0;
-        for(int i = 0 ; i<mat.size();i++){
-            for(int j = 0 ; j < mat[0].size();j++){
-                if(mat[i][j]==1 && isSpecial(mat,i,j)){
+        int rows = mat.size();
+        int cols = mat[0].size();
+        vector<int> rowSum(rows, 0);
+        vector<int> colSum(cols, 0);
+
+        // Calculate row and column sums
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                rowSum[i] += mat[i][j];
+                colSum[j] += mat[i][j];
+            }
+        }
+
+        // Check for special elements
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (mat[i][j] == 1 && rowSum[i] == 1 && colSum[j] == 1) {
                     count++;
-                    break;
                 }
             }
         }
-        return count;
-    }
-    bool isSpecial(vector<vector<int>>& mat,int row,int col){
-        int rowSum = 0;
-        int colSum = 0;
-        for(int i = 0 ; i < mat.size(); i++ ){
-                rowSum += mat[i][col];
-        }
-        
-    for(int i = 0 ; i < mat[0].size(); i++){
-        colSum += mat[row][i];
 
-    }
-    return (rowSum == 1 && colSum == 1);
+        return count;
     }
 };
