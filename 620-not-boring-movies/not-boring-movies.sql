c030a0150403687c2ddd301944a420f0b9a4713a
AishwaryaRajput09 <71898668+AishwaryaRajput09@users.noreply.github.com>
2023-12-15 10:52:14 +0530

    Time: 429 ms (42.45%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 SELECT * FROM Cinema 
-WHERE NOT id % 2 = 0 AND description != 'boring'
+WHERE NOT id % 2 = 0 AND description NOT LIKE "%boring%"
 ORDER BY rating DESC;
