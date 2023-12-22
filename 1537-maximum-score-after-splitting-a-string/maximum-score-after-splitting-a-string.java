f360956ab349d04d1908c259fc129bd7c1dcd5c1
AishwaryaRajput09 <71898668+AishwaryaRajput09@users.noreply.github.com>
2023-12-22 08:06:33 +0530

    Time: 9 ms (33.78%) | Memory: 45.3 MB (6.22%) - LeetSync

---
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int maxScore(String s) {
+        int max = 0;
+        for(int i = 0; i<s.length();i++){
+             int count = 0;
+            String left = s.substring(0,i+1);
+            String right = s.substring(i+1);
+            if(!left.isEmpty()&& !right.isEmpty()){
+
+        count = countZero(left) + countOnes(right);
+            }
+
+        max = Math.max(max,count);
+
+        }
+        return max;
+    }
+    private int countZero(String s){
+        int count = 0; 
+        for(char ch : s.toCharArray()){
+            if(ch == '0'){
+                count++;
+            }
+        }
+        return count;
+    }
+    private int countOnes(String s){
+        int count = 0; 
+        for(char ch : s.toCharArray()){
+            if(ch == '1'){
+                count++;
+            }
+        }
+        return count;
+    }
+}
