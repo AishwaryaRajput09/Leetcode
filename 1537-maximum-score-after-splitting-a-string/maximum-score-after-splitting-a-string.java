03037828301010224d17617caf9330060059be84
AishwaryaRajput09 <71898668+AishwaryaRajput09@users.noreply.github.com>
2023-12-22 08:14:16 +0530

    Time: 1 ms (97.78%) | Memory: 41.5 MB (32.89%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -1,36 +1,56 @@
 class Solution {
     public int maxScore(String s) {
-        int max = 0;
-        for(int i = 0; i<s.length();i++){
-             int count = 0;
-            String left = s.substring(0,i+1);
-            String right = s.substring(i+1);
-            if(!left.isEmpty()&& !right.isEmpty()){
+    //     int max = 0;
+    //     for(int i = 0; i<s.length();i++){
+    //          int count = 0;
+    //         String left = s.substring(0,i+1);
+    //         String right = s.substring(i+1);
+    //         if(!left.isEmpty()&& !right.isEmpty()){
 
-        count = countZero(left) + countOnes(right);
-            }
+    //     count = countZero(left) + countOnes(right);
+    //         }
 
-        max = Math.max(max,count);
+    //     max = Math.max(max,count);
+
+    //     }
+    //     return max;
+    // }
+    // private int countZero(String s){
+    //     int count = 0; 
+    //     for(char ch : s.toCharArray()){
+    //         if(ch == '0'){
+    //             count++;
+    //         }
+    //     }
+    //     return count;
+    // }
+    // private int countOnes(String s){
+    //     int count = 0; 
+    //     for(char ch : s.toCharArray()){
+    //         if(ch == '1'){
+    //             count++;
+    //         }
+    //     }
+    //     return count;
+    int left =0;
+    int right = 0;
+    int score = 0;
+
+    for(char ch : s.toCharArray()){
+        if(ch == '1'){
+            right++;
+        }
+    }
+    for(int i = 0; i < s.length()-1;i++){
+        if(s.charAt(i)=='0'){
+            left++;
+        }else{
+            right--;
 
         }
-        return max;
+            score = Math.max(score,left+right);
     }
-    private int countZero(String s){
-        int count = 0; 
-        for(char ch : s.toCharArray()){
-            if(ch == '0'){
-                count++;
-            }
-        }
-        return count;
+        return score;
     }
-    private int countOnes(String s){
-        int count = 0; 
-        for(char ch : s.toCharArray()){
-            if(ch == '1'){
-                count++;
-            }
-        }
-        return count;
-    }
+
 }
