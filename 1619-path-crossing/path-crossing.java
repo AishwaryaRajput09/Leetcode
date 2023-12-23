98e319e896e2f9102fab8a67e8cc1af02967eb28
AishwaryaRajput09 <71898668+AishwaryaRajput09@users.noreply.github.com>
2023-12-23 08:37:36 +0530

    Time: 5 ms (42.95%) | Memory: 42.5 MB (5.29%) - LeetSync

--- 1619-path-crossing/path-crossing.java
+++ 1619-path-crossing/path-crossing.java
@@ -3,24 +3,30 @@ class Solution {
         int x = 0;
         int y =0;
 
-        Set<String> map = new HashSet<>();
-        map.add("0,0");
-        for(int i = 0; i < path.length();i++){
-            if(path.charAt(i)== 'N'){
-                y += 1;
-            }else if(path.charAt(i)=='S'){
-                y -= 1;
-            }else if(path.charAt(i)=='E'){
-                x += 1;
-            }else if(path.charAt(i)=='W'){
-                x -= 1;
+        Set<String> visited = new HashSet<>();
+        visited.add("0,0");
+        for(char ch : path.toCharArray()){
+            switch(ch){
+                case 'N':
+                y++;
+                break;
+                case 'S':
+                y--;
+                break;
+                case 'E':
+                x++;
+                break;
+                case 'W':
+                x--;
+                break;
             }
+            
             String curr = x + "," + y;
 
-            if(map.contains(curr)){
+            if(visited.contains(curr)){
                 return true;
             }
-            map.add(curr);
+            visited.add(curr);
         }
         return false;
     }
