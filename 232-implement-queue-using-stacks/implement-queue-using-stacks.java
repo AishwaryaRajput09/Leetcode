9d7051923de2c46ea804c7d6e8f4ad78ff954d72
AishwaryaRajput09 <71898668+AishwaryaRajput09@users.noreply.github.com>
2023-12-23 12:35:34 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (5.17%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -1,43 +1,73 @@
 class MyQueue {
-    private Stack<Integer> st1;
-    private Stack<Integer> st2;
+    // private Stack<Integer> st1;
+    // private Stack<Integer> st2;
+    // public MyQueue() {
+    //      st1 = new Stack<>();
+    //      st2 = new Stack<>();
+
+    // }
+    
+    // public void push(int x) {
+    //     st1.push(x);
+    // }
+    
+    // public int pop() {
+    //     if(st2.isEmpty()){
+    //         while(!st1.isEmpty()){
+
+    //         st2.push(st1.pop());
+    //         }
+    //     }
+    //     if(!st2.isEmpty()){
+    //         return st2.pop();
+    //     }
+    //     return 0;
+    // }
+    
+    // public int peek() {
+    //     if(st2.isEmpty()){
+    //         while(!st1.isEmpty()){
+    //             st2.push(st1.pop());
+    //         }
+    //     }
+    //     if(!st2.isEmpty()){
+    //         return st2.peek();
+    //     }
+    //     return 0;
+    // }
+    
+    // public boolean empty() {
+    //     return st1.isEmpty() && st2.isEmpty();
+    // }
+     
+    private Queue<Integer> queue;
     public MyQueue() {
-         st1 = new Stack<>();
-         st2 = new Stack<>();
+         queue = new LinkedList<>();
+        //  st2 = new Stack<>();
 
     }
     
     public void push(int x) {
-        st1.push(x);
+        queue.offer(x);
     }
     
     public int pop() {
-        if(st2.isEmpty()){
-            while(!st1.isEmpty()){
-
-            st2.push(st1.pop());
-            }
-        }
-        if(!st2.isEmpty()){
-            return st2.pop();
+        if(!queue.isEmpty()){
+            return queue.poll();
         }
         return 0;
     }
     
     public int peek() {
-        if(st2.isEmpty()){
-            while(!st1.isEmpty()){
-                st2.push(st1.pop());
-            }
+        if(!queue.isEmpty()){
+            return queue.peek();
         }
-        if(!st2.isEmpty()){
-            return st2.peek();
-        }
+        
         return 0;
     }
     
     public boolean empty() {
-        return st1.isEmpty() && st2.isEmpty();
+        return queue.isEmpty();
     }
 }
 
