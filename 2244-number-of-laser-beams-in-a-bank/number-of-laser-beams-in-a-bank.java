9ff7bd5f6eefe5974ab32e6532a15ce0e6ecce4e
AishwaryaRajput09 <71898668+AishwaryaRajput09@users.noreply.github.com>
2024-01-03 18:53:48 +0530

    Time: 11 ms (92.06%) | Memory: 45.2 MB (15.28%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
@@ -3,7 +3,12 @@ class Solution {
         int prevRow  = 0;
         int sum = 0;
       for(String s: bank){
-        int currRow = calc(s);
+        int currRow = 0;
+        for(char ch : s.toCharArray()){
+              if(ch == '1'){
+                  currRow++;
+              }
+          }
         if(currRow == 0){
             continue;
         }
@@ -13,13 +18,5 @@ class Solution {
       }  
       return sum;
     }
-    int calc(String s){
-       int count = 0;
-        for(char ch : s.toCharArray()){
-              if(ch == '1'){
-                  count++;
-              }
-          }
-          return count;
-    }
+
 }
