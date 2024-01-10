856ca5c25546ca064dca0eaeaa57e6188276e4b9
AishwaryaRajput09 <71898668+AishwaryaRajput09@users.noreply.github.com>
2024-01-10 21:00:23 +0530

    Time: 7 ms (99.70%) | Memory: 77.5 MB (64.72%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
@@ -1,81 +1,25 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public static TreeNode parent_link(TreeNode root,Map<TreeNode,TreeNode>map,int start){
-		Queue<TreeNode>q=new LinkedList<>();
-		TreeNode res=null;
-		q.add(root);
+  private int ans;
 
-		while(!q.isEmpty()){
-			TreeNode cur=q.poll();
-			if(cur.val==start) res=cur;
+  public int amountOfTime(TreeNode root, int start) {
+    dfs(root, start);
+    return ans;
+  }
 
-			if(cur.left!=null){
-				map.put(cur.left,cur);
-				q.add(cur.left);
-			}
+  public int dfs(TreeNode root, int start) {
+    if (root == null) return 0;
 
-			if(cur.right!=null){
-				map.put(cur.right,cur);
-				q.add(cur.right);
-			}
-		}
-		return res;
-	}
+    int leftDepth = dfs(root.left, start);
+    int rightDepth = dfs(root.right, start);
 
-    public int amountOfTime(TreeNode root, int start) {
-        Map<TreeNode,TreeNode>parent_map=new HashMap<>();
-        TreeNode target=parent_link(root,parent_map,start);
-		
-		Map<TreeNode,Boolean>visit=new HashMap<>();
-		Queue<TreeNode>qu=new LinkedList<>();
-
-		visit.put(target,true);
-		qu.add(target);
-
-		int minTime=0;
-		while(!qu.isEmpty()){
-			int size=qu.size();
-
-			int flag=0;
-
-			for(int i=0;i<size;i++){
-				TreeNode cur=qu.poll();
-
-				if(parent_map.get(cur)!=null && visit.get(parent_map.get(cur))==null){
-					flag=1;
-					visit.put(parent_map.get(cur),true);
-					qu.add(parent_map.get(cur));
-				}
-
-				if(cur.left!=null && visit.get(cur.left)==null){
-					flag=1;
-					visit.put(cur.left,true);
-					qu.add(cur.left);
-				}
-
-				if(cur.right!=null && visit.get(cur.right)==null){
-					flag=1;
-					visit.put(cur.right,true);
-					qu.add(cur.right);
-				}
-			}
-
-			if(flag==1) minTime++;
-		}
-    return minTime;
+    if (root.val == start) {
+      ans = Math.max(leftDepth, rightDepth);
+      return -1;
+    } else if (leftDepth >= 0 && rightDepth >= 0) {
+      return Math.max(leftDepth, rightDepth) + 1;
+    } else {
+      ans = Math.max(ans, Math.abs(leftDepth - rightDepth));
+      return Math.min(leftDepth, rightDepth) - 1;
     }
+  }
 }
