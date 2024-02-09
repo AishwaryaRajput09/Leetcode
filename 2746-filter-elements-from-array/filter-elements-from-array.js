6954c0329a6abb76966403f8b1dd64cd053258a7
AishwaryaRajput09 <71898668+AishwaryaRajput09@users.noreply.github.com>
2024-02-09 11:19:18 +0530

    Time: 58 ms (28.95%) | Memory: 49.1 MB (12.82%) - LeetSync

--- 2746-filter-elements-from-array/filter-elements-from-array.js
+++ 2746-filter-elements-from-array/filter-elements-from-array.js
@@ -4,5 +4,11 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    return arr.filter(fn);
+    const filterd = [];
+    for(let i = 0 ; i < arr.length; i++){
+        if(fn(arr[i],i)){
+            filterd.push(arr[i]);
+        }
+    }
+    return filterd;
 };
