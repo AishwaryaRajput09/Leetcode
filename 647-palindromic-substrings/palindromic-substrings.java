930441bacfb454beab1d658fd076dcfd72f313e5
AishwaryaRajput09 <71898668+AishwaryaRajput09@users.noreply.github.com>
2024-02-10 14:56:44 +0530

    Time: 12 ms (31.89%) | Memory: 44.9 MB (31.35%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -24,19 +24,53 @@
 //         return true;
 //     }
 // }
+// class Solution {
+//     public int countSubstrings(String s) {
+//         int count = 0;
+//         int n = s.length();
+
+//         for (int i = 0; i < 2 * n - 1; i++) {
+//             int left = i / 2;
+//             int right = left + i % 2;
+
+//             while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+//                 count++;
+//                 left--;
+//                 right++;
+//             }
+//         }
+
+//         return count;
+//     }
+// }
 class Solution {
     public int countSubstrings(String s) {
-        int count = 0;
         int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int count = 0;
 
-        for (int i = 0; i < 2 * n - 1; i++) {
-            int left = i / 2;
-            int right = left + i % 2;
+        // All individual characters are palindromes
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+            count++;
+        }
 
-            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+        // Check for palindromic substrings of length 2
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
                 count++;
-                left--;
-                right++;
+            }
+        }
+
+        // Check for palindromic substrings of length 3 or more
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    count++;
+                }
             }
         }
 
