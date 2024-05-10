5a8b79fe313bffcb14140a4f671e389c910cfc69
AishwaryaRajput09 <71898668+AishwaryaRajput09@users.noreply.github.com>
2024-05-10 10:39:57 +0530

    Time: 766 ms (6.11%) | Memory: 68.5 MB (54.58%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
@@ -1,39 +1,17 @@
-import java.util.*;
-
-class Fraction implements Comparable<Fraction> {
-    int numerator;
-    int denominator;
-
-    public Fraction(int numerator, int denominator) {
-        this.numerator = numerator;
-        this.denominator = denominator;
-    }
-
-    @Override
-    public int compareTo(Fraction other) {
-        // Implement comparison logic
-        return this.numerator * other.denominator - other.numerator * this.denominator;
-    }
-}
-
 class Solution {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        Map<Fraction,int[]> map = new TreeMap<>();
-        for(int i = 0; i < arr.length; i++){
-            for(int j = i + 1; j < arr.length; j++){
-                map.put(new Fraction(arr[i],arr[j]),new int[]{arr[i],arr[j]});
+        int n = arr.length;
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingDouble((int[]a) -> (double) arr[a[0]]/arr[a[1]]));
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                minHeap.offer(new int[]{i, j});
             }
         }
-        Map<Fraction,int[]> sortedMap = new TreeMap<>(map);
-        int count = 0;
-        int[] res = null;
-        for(int[] values : sortedMap.values()){
-            count++;
-            if(count == k){
-                res = values;
-                break;
-            }
+        while(k > 1){
+            minHeap.poll();
+            k--;
         }
-        return res;
+        int[] result = minHeap.poll();
+        return new int[]{arr[result[0]],arr[result[1]]};
     }
 }
