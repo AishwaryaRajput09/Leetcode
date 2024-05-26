97d75e642c546e172854ab78849b1ff5fbf05e03
AishwaryaRajput09 <71898668+AishwaryaRajput09@users.noreply.github.com>
2024-05-26 10:22:56 +0530

    Time: 1 ms (53.38%) | Memory: 40.3 MB (47.53%) - LeetSync

---
+++ 2507-number-of-common-factors/number-of-common-factors.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int commonFactors(int a, int b) {
+        int cnt = 0;
+        int max = Math.max(a,b);
+        for(int i = 1; i <= max; i++){
+            if(a%i == 0 && b % i == 0){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
