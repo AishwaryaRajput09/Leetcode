fc11863af759085c7a7f93c06ad2d63c58013c88
AishwaryaRajput09 <71898668+AishwaryaRajput09@users.noreply.github.com>
2024-06-07 19:44:32 +0530

    Time: 1 ms (9.26%) | Memory: 41.4 MB (70.14%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -15,23 +15,46 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
+    // if(this sol){
+    //     List<Integer> list = new ArrayList<>();
+    //     Stack<TreeNode> st1 = new Stack<>();
+    //     Stack<TreeNode> st2 = new Stack<>();
+    //     // TreeNode node = root;
+    //     if(root == null){
+    //         return list;
+    //     }
+    //              while(!st1.isEmpty()){
+    //         root = st1.pop();
+    //         st2.add(root);
+    //         if(root.left != null) st1.push(root.left);
+    //         if(root.right != null) st1.push(root.right);
+    //     }
+    //         while(!st2.isEmpty()){
+    //             list.add(st2.pop().val);
+    //         }
+    // }
+    //     return list;
+    
         List<Integer> list = new ArrayList<>();
-        Stack<TreeNode> st1 = new Stack<>();
-        Stack<TreeNode> st2 = new Stack<>();
-        // TreeNode node = root;
-        if(root == null){
-            return list;
-        }
-        st1.push(root);
-        while(!st1.isEmpty()){
-            root = st1.pop();
-            st2.add(root);
-            if(root.left != null) st1.push(root.left);
-            if(root.right != null) st1.push(root.right);
-        }
-            while(!st2.isEmpty()){
-                list.add(st2.pop().val);
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode node = root;
+        TreeNode lastVisit = null;
+        while(!st.isEmpty() || node != null){
+            if(node != null){
+                st.push(node);
+                node = node.left;
+            }else{
+                TreeNode peekNode = st.peek();
+                if(peekNode.right != null && lastVisit != peekNode.right){
+                    node = peekNode.right;
+                }else{
+                    st.pop();
+                    list.add(peekNode.val);
+                    lastVisit = peekNode;
+                }
             }
+            
+        }
         return list;
     }
 }
