cfe123558af7a1b268b99d3bcc0931d8dfef54c0
AishwaryaRajput09 <71898668+AishwaryaRajput09@users.noreply.github.com>
2024-07-10 13:38:57 +0530

    Time: 9 ms (44.79%) | Memory: 42.8 MB (5.90%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.java
+++ 1720-crawler-log-folder/crawler-log-folder.java
@@ -1,22 +1,39 @@
 
 class Solution {
     public int minOperations(String[] logs) {
-        String patS ="^[a-z]+\\d+/$";
-        String patS2 = "../";
-        Stack<String> st = new Stack<>();
-        for(int i = 0; i < logs.length; i++){
-            if(logs[i].matches(patS)){
-                st.push(logs[i]);
-            }else if(logs[i].matches(patS2)){
-                if(!st.isEmpty()){
-                st.pop();
+        // String patS ="^[a-z]+\\d+/$";
+        // String patS2 = "../";
+        // Stack<String> st = new Stack<>();
+        // for(int i = 0; i < logs.length; i++){
+        //     if(logs[i].matches(patS)){
+        //         st.push(logs[i]);
+        //     }else if(logs[i].matches(patS2)){
+        //         if(!st.isEmpty()){
+        //         st.pop();
+        //         }
+        //         System.out.println(st); 
+        //     }else if(logs[i].matches("1/")){
+        //         st.push(logs[i]);
+        //     }
+        // }
+                
+        // return st.size();
+        int steps = 0;
+        for(String s: logs){
+            if(s.equals("../")){
+                    System.out.println(0);
+                if(steps == 0){
+                    continue;
+                    }
+                else {
+                    steps--;
                 }
-                System.out.println(st); 
-            }else if(logs[i].matches("1/")){
-                st.push(logs[i]);
+            }else if(s.equals("./")){
+                continue;
+            }else{
+                steps++;
             }
         }
-                
-        return st.size();
+        return steps;
     } 
 }
