a50ed68200a6c530606a9714b0245522d7ca3329
AishwaryaRajput09 <71898668+AishwaryaRajput09@users.noreply.github.com>
2024-08-13 11:32:50 +0530

    Time: 5 ms (21.54%) | Memory: 43.2 MB (54.32%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,30 +1,29 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
         List<Integer> ans = new ArrayList<>();
         int n = candidates.length;
         Arrays.sort(candidates);
-        Sum2(candidates,res,ans,0,0,target,n);
-        return res;
-    }
-    List<List<Integer>> Sum2(int[] arr,List<List<Integer>> res,List<Integer> ans,int sum,int i,int target,int n){
-        if(sum == target){
-            res.add(new ArrayList<>(ans));
-            return res;
-        }
-        if(i==n|| sum>target){
-            return res;
-        }
-        ans.add(arr[i]);
-       
-        Sum2(arr,res,ans, sum += arr[i],i+1,target,n);
-        while (i < n - 1 && arr[i] == arr[i + 1]) {
-            i++;
-        }
-        ans.remove(ans.size()-1);
-        sum -= arr[i];
-        Sum2(arr,res,ans,sum,i+1,target,n);
+        sumComb2(result,ans,0,0,target,candidates,n);
+        return result;
 
-        return res;
+    }
+    public List<List<Integer>> sumComb2(List<List<Integer>> result,List<Integer> ans,int i, int sum,int target,int[] arr,int n){
+            if(sum == target){
+                result.add(new ArrayList<>(ans));
+                return result;
+            }
+            if(i == n || sum > target){
+                return result;
+            }
+            ans.add(arr[i]);
+            sumComb2(result,ans,i+1,sum+= arr[i],target,arr,n);
+            while(i < n - 1 && arr[i] == arr[i+1] ){
+                i++;
+            }
+            ans.remove(ans.size()-1);
+            sum -= arr[i];
+            sumComb2(result,ans,i+1,sum,target,arr,n);
+            return result;
     }
 }
