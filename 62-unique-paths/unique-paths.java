c6c96999c91441020c7b69053c60bce2f73d1f91
AishwaryaRajput09 <71898668+AishwaryaRajput09@users.noreply.github.com>
2025-05-29 15:33:29 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (61.56%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,18 +1,17 @@
+import java.util.Arrays;
 class Solution {
     public int uniquePaths(int m, int n) {
-        HashMap<String,Integer> map = new HashMap<>();
-
-        return helper(m,n,map);
-        
-        
+        int[][] memo = new int[m+1][n+1];
+        for(int[]row:memo){
+            Arrays.fill(row, -1);
+        }
+        return helper(m, n, memo);
     }
-    int helper(int m, int n, HashMap<String,Integer> memo){
-        String key = m + "," + n;
-        if(memo.containsKey(key)) return memo.get(key);
+    int helper(int m, int n, int[][] memo){
         if(m == 1 && n == 1) return 1;
         if(m == 0 || n == 0) return 0;
-        int res = helper(m-1,n,memo) + helper(m,n-1,memo);
-        memo.put(key,res);
-        return res;
+        if(memo[m][n] != -1) return memo[m][n];
+        memo[m][n] = helper(m-1,n, memo) + helper(m,n-1,memo);
+        return memo[m][n];
     }
 }
