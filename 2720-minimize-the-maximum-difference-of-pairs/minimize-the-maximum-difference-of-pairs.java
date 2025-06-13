7573f63e3bf1ff5477740f1c0df22f951f0ee115
Aishwarya <71898668+AishwaryaRajput09@users.noreply.github.com>
2025-06-13 23:24:19 +0530

    Time: 17 ms (78.00%) | Memory: 55.8 MB (97.00%) - LeetSync

---
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minimizeMax(int[] nums, int p) {
+        Arrays.sort(nums);
+        int low = 0;
+        int ans = 0;
+        int high = nums[nums.length - 1] - nums[0];
+        while(high >= low){
+            int mid = low + (high - low) / 2;
+            int cnt = 0;
+            for(int i = 1; i < nums.length; i++){
+                if(nums[i] - nums[i-1] <= mid){
+                    cnt++;
+                    i++;
+                }
+            }
+            if(cnt >= p){
+                ans = mid;
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+
+        }
+        return ans;
+    }
+}
