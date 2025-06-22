e9dac45520ce8af4ea496bd7d9f4846753cc2dd7
Aishwarya <71898668+AishwaryaRajput09@users.noreply.github.com>
2025-06-22 12:16:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (53.61%) - LeetSync

--- 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.java
+++ 2260-divide-a-string-into-groups-of-size-k/divide-a-string-into-groups-of-size-k.java
@@ -1,22 +1,31 @@
 class Solution {
     public String[] divideString(String s, int k, char fill) {
-        int len = s.length();
-        int toFill = k -(len % k);
-        int arrLen = 0;
-        if(len % k != 0){
-        arrLen = (len/k) + 1;
+        // int len = s.length();
+        // int toFill = k -(len % k);
+        // int arrLen = 0;
+        // if(len % k != 0){
+        // arrLen = (len/k) + 1;
 
-        for(int i = 0; i < toFill; i++){
-            s += fill;
+        // for(int i = 0; i < toFill; i++){
+        //     s += fill;
+        // }
+        // }else{
+        //     arrLen = (len/k);
+        // }
+        // String[] sArr = new String[arrLen];
+        // int j = 0;
+        // for(int i = 0; i < s.length() && i+k <= s.length(); i+=k){
+        //     sArr[j++] = s.substring(i,i+k); 
+        // }
+        StringBuilder str = new StringBuilder(s);
+
+        while(str.length()%k != 0){
+            str.append(fill);
         }
-        }else{
-            arrLen = (len/k);
-        }
-        String[] sArr = new String[arrLen];
+        String[] sArr = new String[str.length()/k];
         int j = 0;
-        for(int i = 0; i < s.length() && i+k <= s.length(); i+=k){
-            sArr[j++] = s.substring(i,i+k);
-           
+        for(int i = 0; i < str.length(); i+= k){
+            sArr[j++] = str.substring(i, i+k);
         }
         return sArr;
     }
