674f026297b48a5a514773e0688488796b982316
Aishwarya <71898668+AishwaryaRajput09@users.noreply.github.com>
2025-08-31 13:57:59 +0530

    Time: 232 ms (51.15%) | Memory: 41.3 MB (43.93%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -3,37 +3,48 @@ class Solution {
         solve(board);
     }
     boolean solve(char[][] board){
-        for(int i = 0; i < board.length;i++){
-            for(int j = 0; j < board[0].length;j++){
-                if(board[i][j]=='.'){
-                    for(char ch = '1';ch <= '9';ch++){
-                        if(isValid(board,i,j,ch)){
-                            board[i][j]=ch;
-                            if(solve(board)==true){
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] == '.'){
+                    for(char ch ='1'; ch <= '9'; ch++){
+                        if(rowCol(ch,board, i, j) &&  gridWise(ch,board,i,j)){
+                            board[i][j] = ch;
+                            if(solve(board)){
                                 return true;
                             }else{
-                                board[i][j]='.';
+                                board[i][j] = '.';
                             }
                         }
                     }
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+    boolean rowCol(char ch, char[][] mat, int row, int col){
+        //rowwise
+        for(int i = 0; i < 9; i++){
+            if(i != row && mat[i][col] ==ch){
+                return false;
+            }
+            if(i!= col && mat[row][i] ==ch){
+                return false;
+            }
+        }
+        //columnwise
+        return true;
+    }
+    boolean gridWise(char ch, char[][] grid, int row, int col){
+        int startR = (row/3)*3;
+        int startC = (col/3)*3;
+        for(int i = startR; i < startR+3; i++){
+            for(int j = startC; j< startC+3; j++){
+                if((i != row && j != col )&&  grid[i][j]==ch){
                     return false;
                 }
             }
         }
-            return true;
-    }
-    boolean isValid(char[][]board,int row,int col,int ch){
-        for(int i = 0; i<9;i++){
-            if(board[i][col]==ch){
-                return false;
-            }
-            if(board[row][i]==ch){
-                return false;
-            }
-            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==ch){
-                return false;
-            }
-        }
-            return true;
+        return true;
     }
 }
