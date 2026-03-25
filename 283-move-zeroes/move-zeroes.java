7ca83e481b8ab977f5a0a5b5d10d535f0600f25f
Aishwarya <71898668+AishwaryaRajput09@users.noreply.github.com>
2026-03-25 22:21:23 +0530

    Time: 3 ms (18.61%) | Memory: 48.1 MB (14.46%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -3,20 +3,19 @@ class Solution {
         int i = 0;
         int j = 0;
         while(i < nums.length && j < nums.length){
-
-                if(nums[i] == 0){
-                    i++;
-                }else{
-                    swap(i,j,nums);
-                    i++;
-                    j++;
-                }
+            if(nums[i] != 0){
+                swap(i, j, nums);
+                i++;
+                j++;
+            }else{
+                i++;
+            }
+        
         }
-            
     }
-    private void swap(int i, int j, int[] nums){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    public static void swap(int i, int j, int[] arr){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
