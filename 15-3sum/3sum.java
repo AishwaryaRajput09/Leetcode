90fd2922b42a225dd956fed7c49a2bcdaaf96a5c
Aishwarya <71898668+AishwaryaRajput09@users.noreply.github.com>
2026-04-10 19:27:08 +0530

    Time: 1204 ms (5.27%) | Memory: 62.2 MB (6.45%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,25 +1,45 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
+        // a + b + c = 0   0 -> a + b = -c
+        HashSet<List<Integer>> triplets = new HashSet<>();
+        int n = nums.length - 1;
         Arrays.sort(nums);
-        for (int k = 0; k < nums.length - 2; k++) {
-            if (k > 0 && nums[k] == nums[k - 1]) continue;
-            int i = k + 1, j = nums.length - 1;
-            while (i < j) {
-                int sum = nums[k] + nums[i] + nums[j];
-                if (sum == 0) {
-                    res.add(Arrays.asList(nums[k], nums[i], nums[j]));
-                    i++;
-                    j--;
-                    while (i < j && nums[i] == nums[i - 1]) i++;
-                    while (i < j && nums[j] == nums[j + 1]) j--;
-                } else if (sum < 0) {
-                    i++;
-                } else {
-                    j--;
-                }
+        for (int i = 0; i <= n; i++) {
+            if (nums[i] > 0)
+                break;
+
+            List<List<Integer>> pairs = two_sorted_sum(nums, i + 1, n, -nums[i]);
+            for (List<Integer> pair : pairs) {
+                List<Integer> triplet = new ArrayList<>();
+                triplet.add(nums[i]);
+                triplet.addAll(pair);
+                Collections.sort(triplet);
+                triplets.add(triplet);
+            }
+
+        }
+        return new ArrayList<>(triplets);
+    }
+
+    public List<List<Integer>> two_sorted_sum(int[] nums, int start, int end, int target){
+        List<List<Integer>> pairs = new ArrayList<>();
+        while(start < end){
+            List<Integer> pair = new ArrayList<>();
+            int sum = nums[start] + nums[end];
+            if(sum == target){
+                pair.add(nums[start]);
+                pair.add(nums[end]);
+                // pair.add(target);
+                pairs.add(pair);
+                start++;
+               
+                // return pairs;
+            }else if(sum < target){
+                start++;
+            }else{
+                end--;
             }
         }
-        return res;
-    }
+    return pairs;
+       }
 }
