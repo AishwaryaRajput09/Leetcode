661cc5fb7b88eef08f22437cf5cb550d43dfb501
Aishwarya <71898668+AishwaryaRajput09@users.noreply.github.com>
2026-04-11 20:13:02 +0530

    Time: 3 ms (52.91%) | Memory: 46.6 MB (48.42%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,44 +1,43 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean falg = true;
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; j++){
-                if(board[i][j] != '.'){
-              if(!columnWise(board[i][j], i, j,board))return false;
-              if(!rowWise(board[i][j], i, j,board))return false;
-              if(!gridWise(board[i][j], i, j,board))return false;
+        //first let's intialize hashset to store row and colums we will need 9 hashset each so lets just make arraylist of hashset
+        ArrayList<HashSet<Character>> set_of_rows = new ArrayList<>();
+        ArrayList<HashSet<Character>> set_of_cols = new ArrayList<>();
+        // and next the grid as we have 9 subgrids we will be taking 3 row as 0 the second 3 as 1 and same goes for colums
+        HashSet<Character>[][] set_of_grids = new HashSet[3][3];
+        for(int i = 0 ; i < 9; i++){
+            set_of_rows.add(new HashSet<>());
+            set_of_cols.add(new HashSet<>());
+        }
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                set_of_grids[i][j] = new HashSet<>();
+            }
+        }
+
+        for(int r = 0; r < 9; r++){
+            for(int c = 0; c < 9; c++){
+                char ch = board[r][c];
+                if(ch == '.'){
+                    continue;
                 }
-            }
-        }
-        return true;
-    }
-    boolean columnWise(char c, int row, int col, char[][] grid){
-        for(int j = 0; j < 9; j++){
-            if(j != row && grid[j][col] == c){
-                return false;
-            }
-        }
-        return true;
-    }
-    boolean rowWise(char c, int row, int col, char[][] grid){
-        for(int j = 0; j < 9; j++){
-            if(j != col && grid[row][j] == c){
-                return false;
-            }
-        }
-        return true;
-    }
-    boolean gridWise(char c, int row, int col, char[][] grid){
-        int startR = (row / 3) * 3;
-        int startCol = (col / 3) * 3;
-        for(int i = startR; i < startR+3; i++){
-            for(int j = startCol; j < startCol+3; j++){
-                if((row != i && col != j) && grid[i][j] == c){
+
+                if(set_of_rows.get(r).contains(ch)){
                     return false;
                 }
+
+                if(set_of_cols.get(c).contains(ch)){
+                    return false;
+                }
+
+                if(set_of_grids[r / 3][c / 3].contains(ch)){
+                    return false;
+                }
+                set_of_rows.get(r).add(ch);
+                set_of_cols.get(c).add(ch);
+                set_of_grids[r / 3][c / 3].add(ch);
             }
         }
         return true;
     }
-
 }
