e255ea5abe88961ef486ce58160b377ecae61a6a
Aishwarya <71898668+AishwaryaRajput09@users.noreply.github.com>
2026-04-12 10:09:13 +0530

    Time: 1 ms (81.60%) | Memory: 47.7 MB (30.52%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,24 +1,6 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        // HashSet<Integer> zero_rows = new HashSet<>();
-        // HashSet<Integer> zero_cols = new HashSet<>();
-        // for (int r = 0; r < matrix.length; r++) {
-        //     for (int c = 0; c < matrix[0].length; c++) {
-        //         if (matrix[r][c] == 0) {
-        //             zero_rows.add(r);
-        //             zero_cols.add(c);
-        //         }
-        //     }
-        // }
-        // for (int r = 0; r < matrix.length; r++) {
-        //     for (int c = 0; c < matrix[0].length; c++) {
-        //         if(zero_rows.contains(r) || zero_cols.contains(c)){
-        //             matrix[r][c] = 0;
-        //         }
-        //     }
-        // }
-
-        // If we use hashset it takes extra space of(m+n) row , and col m(represents number of rows that contains 0 and n is for cols that contains 0) to optimize it further what we can do ??  I read that we can actually mark the row and cols which contains 0 without hashSet or extra matrix so how can we achieve that it is simple just make 0th col and 0th row as your marker so whenever any col contains 0 we mark the 0th row and 0th col as 0 but for taking care of that we also need to take care of the first row or col already has 0 if it has then it would be bluder to mark them. so, before marking them for entire matrix we will check if they contains 0 or no with taking boolean flags
+        
 
         int m = matrix.length;
         int n = matrix[0].length;
