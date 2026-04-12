d2eeec0862b7d0f081689693c490a6c1036f109a
Aishwarya <71898668+AishwaryaRajput09@users.noreply.github.com>
2026-04-12 08:48:13 +0530

    Time: 4 ms (10.31%) | Memory: 47.6 MB (30.52%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,30 +1,24 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        boolean[][] mat = new boolean[matrix.length][matrix[0].length];
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[0].length; j++){
-                if(matrix[i][j] == 0){
-                    mat[i][j] = true;
-                }else{
-                    mat[i][j] = false;
+        HashSet<Integer> zero_rows = new HashSet<>();
+        HashSet<Integer> zero_cols = new HashSet<>();
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[0].length; c++) {
+                if (matrix[r][c] == 0) {
+                    zero_rows.add(r);
+                    zero_cols.add(c);
                 }
             }
         }
-        for(int i = 0; i < mat.length; i++){
-            for(int j = 0; j < mat[0].length; j++){
-                if(mat[i][j] == true){
-                    convertToZero(matrix, i, j);
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[0].length; c++) {
+                if(zero_rows.contains(r)){
+                    matrix[r][c] = 0;
+                }
+                if(zero_cols.contains(c)){
+                    matrix[r][c] = 0;
                 }
             }
         }
-
     }
-   void convertToZero(int[][] mat, int row, int col){
-     for(int k = 0; k < mat.length; k++){
-        mat[k][col] = 0;
-     }
-     for(int k = 0; k < mat[0].length;k++){
-        mat[row][k] = 0;
-     }
-   }
 }
