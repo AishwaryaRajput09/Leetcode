bc0a66b7ee9ee0d69692ca4ad59ba91cdfb5a340
Aishwarya <71898668+AishwaryaRajput09@users.noreply.github.com>
2026-04-25 18:34:47 +0530

    Time: 1 ms (99.94%) | Memory: 52.7 MB (63.79%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -1,40 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode temp = headA;
+        int len1 = 0;
+        int len2 = 0;
+        ListNode temp1 = headA;
         ListNode temp2 = headB;
-        int size1 = 0, size2 = 0;
-
-        // Count size of the first list
-        while (temp != null) {
-            size1++;
-            temp = temp.next;
+        while(temp1!= null){
+            len1++;
+            temp1 = temp1.next;
         }
-
-        // Count size of the second list
-        while (temp2 != null) {
-            size2++;
+        while(temp2!= null){
+            len2++;
             temp2 = temp2.next;
         }
-
-        // Reset temp and temp2 to the heads
-        temp = headA;
+        int diff = 0;
+        temp1 = headA;
         temp2 = headB;
-
-        // Advance the pointer of the longer list
-        for (int i = 0; i < Math.abs(size1 - size2); i++) {
-            if (size1 > size2) {
-                temp = temp.next;
-            } else {
+        if(len1 > len2){
+            diff = len1 - len2;
+            for(int i = 0; i < diff; i++){
+                temp1 = temp1.next;
+            }
+        }else{
+            diff = len2 - len1;
+            for(int i = 0; i < diff; i++){
                 temp2 = temp2.next;
             }
         }
-
-        // Find the intersection node
-        while (temp != temp2) {
-            temp = temp.next;
+        while(temp1 != temp2){
+            temp1 = temp1.next;
             temp2 = temp2.next;
+        
         }
-
-        return temp; // Either temp or temp2 can be returned, as they now point to the intersection node.
+        return temp1;
     }
 }
