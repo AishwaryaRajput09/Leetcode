a2b8d16bbf8e01893bcad75f0fe22c94349b59b2
Aishwarya <71898668+AishwaryaRajput09@users.noreply.github.com>
2026-04-29 10:08:24 +0530

    Time: 31 ms (31.91%) | Memory: 47.8 MB (15.49%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,48 +1,49 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
-        int k = p.length();
-        if(s.length() < k){
-            return list;
-        }
-        for(char ch : p.toCharArray()){
-            if(map.containsKey(ch)){
-                map.put(ch,map.get(ch)+1);
-            }else{
-                map.put(ch,1);
+        List<Integer> res = new ArrayList<>();
+        HashMap<Character, Integer> freq = new HashMap<>();
+
+        for (char ch : p.toCharArray()) {
+            if (freq.containsKey(ch)) {
+                freq.put(ch, freq.get(ch) + 1);
+            } else {
+                freq.put(ch, 1);
             }
         }
-        int cnt = map.size();
+
+        int cnt = freq.size();
+        int k = p.length();
+        int len = s.length();
         int i = 0;
         int j = 0;
-        while( j < s.length()){
-             char c = s.charAt(j);
-            if(map.containsKey(c)){
-                map.put(c,map.get(c)-1);
-                if(map.get(c)==0){
-                        cnt--;
-                    }
-            }
-            if(j - i + 1 < k){
+        while (j < len) {
+            char c = s.charAt(j);
+            if (freq.containsKey(c)) {
+                freq.put(c, freq.get(c) - 1);
+                if (freq.get(c) == 0) {
+                    // map.remove(c);
+                    cnt--;
+                }
+
+            } 
+            if (j - i + 1 < k) {
                 j++;
-            }
-           else if(j - i + 1  == k){
-                if(cnt == 0){
-                    list.add(i);
-                }
+            } else if (j - i + 1 == k) {
+                if (cnt == 0)
+                    res.add(i);
                 char ch = s.charAt(i);
-            if(map.containsKey(ch)){
-                map.put(ch,map.get(ch)+1);
-                if(map.get(ch) == 1){
-                    cnt++;
+                if (freq.containsKey(ch)) {
+                    freq.put(ch, freq.get(ch) + 1);
+                    if (freq.get(ch) == 1) {
+                        cnt++;
+                    }
                 }
+                i++;
+        j++;
             }
-            j++;
-            i++;
         
-            }
         }
-        return list;
+        return res;
     }
+
 }
