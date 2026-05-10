26baee42931551ca7c7b1aca41414194fba18a1f
Aishwarya <71898668+AishwaryaRajput09@users.noreply.github.com>
2026-05-10 19:00:14 +0530

    Time: 29 ms (35.62%) | Memory: 83 MB (69.51%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,51 +1,58 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        // int s = 0;
-        int e = nums.length - 1;
-        mergeS(nums,0,e);
+        int e = nums.length-1;
+        mergeSort(0,e,nums);
         return nums;
     }
-    public void mergeS(int[] arr, int s, int e) {
-        if (s >= e) {
-            return;
-        }
-        int mid =(e + s) / 2;
-        mergeS(arr, s, mid);
-        mergeS(arr, mid+1, e);
-        merge(arr, s, e);
-    }
+    public void mergeSort(int start, int end, int[] nums){
+        if(start >= end) return;
+        int mid = (start + end)/2;
 
-    private void merge(int[] arr, int s, int e) {
-        int mid = (e + s) / 2;
+        mergeSort(start, mid, nums);
+        mergeSort(mid+1, end, nums);
+
+        merge(nums, start, end);
+    }
+    public void merge(int[] nums, int s, int e){
+
+        int mid = (s + e)/2;
+
         int l1 = mid - s + 1;
         int l2 = e - mid;
-        int[] left = new int[l1];
-        int[] right = new int[l2];
+
+        int[] L = new int[l1];
+        int[] R = new int[l2];
+
         int k = s;
-        for (int i = 0; i < l1; i++) {
-            left[i] = arr[k++];
+        for(int i = 0; i < l1; i++){
+            L[i] = nums[k++];
         }
+
         k = mid + 1;
-        for (int i = 0; i < l2; i++) {
-            right[i] = arr[k++];
+        for(int i = 0; i < l2; i++){
+            // System.out.print( k + " ");
+            R[i] = nums[k++];
         }
 
         k = s;
         int ind1 = 0;
         int ind2 = 0;
-        while(ind1 < left.length && ind2 < right.length){
-            if(left[ind1] < right[ind2]){
-                arr[k++] = left[ind1++];
-            }else{
-                arr[k++] = right[ind2++];
-            }
 
+        while(ind1 < L.length && ind2 < R.length){
+            if(L[ind1] < R[ind2]){
+                nums[k++] = L[ind1];
+                ind1++;
+            }else{
+                nums[k++] = R[ind2];
+                ind2++;
+            }
         }
-        while(ind1 < left.length){
-            arr[k++] = left[ind1++];
+        while(ind1 < L.length){
+            nums[k++] = L[ind1++];
         }
-        while(ind2 < right.length){
-            arr[k++] = right[ind2++];
+        while(ind2 < R.length){
+            nums[k++] = R[ind2++];       
         }
     }
+    
 }
