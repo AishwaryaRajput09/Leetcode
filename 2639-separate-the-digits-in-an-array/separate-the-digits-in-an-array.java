60ab39328b61e651cb3a39ff451325f81cdae8d3
Aishwarya <71898668+AishwaryaRajput09@users.noreply.github.com>
2026-05-11 09:42:48 +0530

    Time: 5 ms (77.69%) | Memory: 46.1 MB (97.23%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.java
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.java
@@ -1,28 +1,30 @@
 class Solution {
     public int[] separateDigits(int[] nums) {
         ArrayList<Integer> digits = new ArrayList<>();
-        for(int num : nums){
-            if(num > 9){
+        for (int num : nums) {
+            if (num > 9) {
                 extractDigit(num, digits);
-            }else{
+            } else {
                 digits.add(num);
             }
         }
         int[] result = new int[digits.size()];
         int ind = 0;
-        for(int num: digits){
+        for (int num : digits) {
             result[ind++] = num;
         }
         return result;
     }
-    public void extractDigit(int num, ArrayList<Integer> digits){
+
+    public void extractDigit(int num, ArrayList<Integer> digits) {
         ArrayList<Integer> rev = new ArrayList<>();
-        while(num != 0){
+        while (num != 0) {
             int rem = num % 10;
             rev.add(rem);
             num /= 10;
         }
-        Collections.reverse(rev);
-        digits.addAll(rev);
+        for (int i = rev.size() - 1; i >= 0; i--) {
+            digits.add(rev.get(i));
+        }
     }
 }
