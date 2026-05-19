8ee1e7d9fc97531122ee61e09738ce127b441944
Aishwarya <71898668+AishwaryaRajput09@users.noreply.github.com>
2026-05-19 07:32:20 +0530

    Time: 0 ms (100.00%) | Memory: 80.5 MB (48.43%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -1,19 +1,22 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> freq = new HashMap<>();
-        for(int num : nums1){
-            if(freq.containsKey(num)){
-            freq.put(num, freq.get(num) + 1);
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+
+        int i = 0;
+        int j = 0;
+        if(nums1[n1-1] < nums2[0] || nums2[n2-1]< nums1[0]){
+            return -1;
+        }
+        while( i < n1 && j < n2){
+            if(nums1[i] == nums2[j]){
+                return nums1[i];
+            }else if(nums1[i] > nums2[j]){
+                j++;
             }else{
-                freq.put(num, 1);
+                i++;
             }
         }
-        int min = Integer.MAX_VALUE;
-        for(int num : nums2){
-            if(freq.containsKey(num)){
-                min = Math.min(min, num);
-            }
-        }
-        return min == Integer.MAX_VALUE ? -1 : min;
+        return -1;
     }
 }
