62fdba29171f8b4d6e47274fe8a8b937f18b1693
Aishwarya <71898668+AishwaryaRajput09@users.noreply.github.com>
2026-05-19 08:24:56 +0530

    Time: 3 ms (55.84%) | Memory: 53 MB (22.92%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        boolean visited[] = new boolean[arr.length];
+        Arrays.fill(visited, false);
+        return solve(arr, start, visited);
+    }
+
+    public boolean solve(int[] arr, int start, boolean[] visited) {
+       if(arr[start] == 0){
+        return true;
+       }
+       boolean left = false;
+       boolean right = false;
+       if(!visited[start]){
+        visited[start] = true;
+        if(start + arr[start] < arr.length){
+        
+        left = solve(arr, start + arr[start], visited);
+        
+       }
+       if (start - arr[start] >= 0 && start - arr[start] < arr.length){
+        right = solve(arr, start - arr[start], visited);
+       }
+    }
+        return left || right ? true: false;
+    }
+}
