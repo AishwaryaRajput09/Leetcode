5729de3f20a7c87f4383d1807da7ede9c14a786d
Aishwarya <71898668+AishwaryaRajput09@users.noreply.github.com>
2026-05-20 09:39:47 +0530

    Time: 3 ms (34.05%) | Memory: 49 MB (19.41%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,30 +1,37 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
-    private static final String[] KEYPAD_MAPPING = {
-        "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
-    };
-
     public List<String> letterCombinations(String digits) {
-        if (digits.isEmpty()) {
-            return new ArrayList<>();
-        }
-        return pad("", digits);
+        List<String> res = new ArrayList<>();
+        HashMap<Character, String> map = new HashMap<>();
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
+        if(digits.length() == 0){
+            return res;
+        } 
+        combinations(digits, res, "", 0, map);
+        return res;
+
     }
 
-    private List<String> pad(String p, String up) {
-        List<String> list = new ArrayList<>();
-        if (up.isEmpty()) {
-            list.add(p);
-            return list;
+    public void combinations(String digits, List<String> res, String op, int ind, HashMap<Character, String> map) {
+        if (op.length() == digits.length()) {
+            res.add(op);
+            return;
         }
-        int digit = up.charAt(0) - '0';
-        String chars = KEYPAD_MAPPING[digit];
-        for (int i = 0; i < chars.length(); i++) {
-            char ch = chars.charAt(i);
-            list.addAll(pad(p + ch, up.substring(1)));
+        char digit = digits.charAt(ind);
+        // digits = digits.substring(1);
+        if(map.containsKey(digit)){
+            String ip = map.get(digit);
+            for(int i = 0; i < ip.length(); i++){
+                char ch = ip.charAt((i));
+                // ip = ip.substring(1);
+                combinations(digits, res, op+ch, ind+1, map);
+            }
         }
-        return list;
     }
 }
