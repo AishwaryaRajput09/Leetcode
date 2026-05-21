415f00015e73083a76fbfa88d4f89d5b62750935
Aishwarya <71898668+AishwaryaRajput09@users.noreply.github.com>
2026-05-21 20:26:42 +0530

    Time: 28 ms (11.24%) | Memory: 53.8 MB (69.54%) - LeetSync

---
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.java
@@ -0,0 +1,55 @@
+class Solution {
+    public String shortestCommonSupersequence(String str1, String str2) {
+        int m = str1.length();
+        int n = str2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0)
+                    dp[i][j] = 0;
+            }
+        }
+        return lcsToShortestCommonSuperSequence(str1, str2, m, n, dp);
+    }
+
+    private String lcsToShortestCommonSuperSequence(String s1, String s2, int m, int n, int[][] dp) {
+        // StringBuilder str = new StringBuilder();
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] =  1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]); 
+                }
+            }
+        }
+         StringBuilder lcs = new StringBuilder();
+        int i = m;
+        int j = n;
+        while(i > 0 && j > 0){
+            if(s1.charAt(i-1) == s2.charAt(j-1)){
+                lcs.append(s1.charAt(i-1));
+                i--;
+                j--;
+            }else{
+                if(dp[i-1][j] > dp[i][j-1]){
+                    lcs.append(s1.charAt(i-1));
+                    i--;
+                }else{
+                    lcs.append(s2.charAt(j-1));
+                    j--;
+                }
+            }
+        }
+        while(i > 0){
+            lcs.append(s1.charAt(i-1));
+            i--;
+        }
+        while(j > 0){
+            lcs.append(s2.charAt(j-1));
+            j--;
+        }
+
+       return lcs.reverse().toString();
+    }
+}
